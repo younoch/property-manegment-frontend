0a71a742bce6b18dda6ee019d0ec608502eb002c
younoch <younoch295@outlook.com>
2025-11-09 00:23:13 +0600

    optimize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
-
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './app/**/*.{vue,js,ts}',
     './pages/**/*.{vue,js,ts}',
-    './components/**/*.{vue,js,ts}'
+    './components/**/*.{vue,js,ts}',
+    './composables/**/*.{js,ts,vue}',
+    './plugins/**/*.{js,ts,vue}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
-      }
-    }
+        inter: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
   corePlugins: {
-    preflight: false,
-  }
+    preflight: false, // disable reset if you manage your own
+    float: false,
+    objectFit: false,
+    divideWidth: false,
+    divideColor: false,
+    divideStyle: false,
+  },
 }
